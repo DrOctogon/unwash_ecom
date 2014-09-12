1cc539cb89cf27423c8066ffd6336b7ea973eaf1
Roberto Aguilar <roberto@baremetal.io>
2014-09-12 15:20:12 -0700

    Updated stores view to be able to select salon

--- unwash/urls.py
+++ unwash/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, url
 
+from unwash.views import SalonListView, SalonSelectView
+
 
 urlpatterns = patterns(
     '',
 
-    url(r'salon$', 'unwash.views.salon'),
+    url(r'salon/$', SalonListView.as_view(), name='salon'),
+    url(r'salon/select/(?P<slug>[^/]+)/(?P<pk>[0-9a-f-]+)/$', SalonSelectView.as_view(), name='select_salon'),
 )
--- unwash/views.py
+++ unwash/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from stores.views import StoreListView, Store
 
 
-def salon(request):
-    ctx = {}
+class SalonListView(StoreListView):
+    template_name = 'unwash/salon.html'
 
-    return render(request, 'unwash/salon.html', ctx)
+
+class SalonSelectView(DetailView):
+    def get(self, request, *args, **kwargs):
+        store = Store.objects.get(pk=kwargs['pk'])
+
+        raise Exception(store)
+
+        return super(SalonSelectView, self).get(request, *args, **kwargs)
